a06d9b6acdd3b078bf94672b5cea3e99cfdf926d
a888744s <jirachtle@gmail.com>
2023-08-18 16:52:01 +0700

    edit 'notify' in quasar.config

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -8,10 +8,10 @@
       <q-input
         filled
         v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        label="ชื่อ-สกุล *"
+        hint="ชื่อเเละนามสกุล"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        :rules="[ val => val && val.length > 0 || 'กรุณาใส่ชื่อสกุล']"
       />
       <q-input
         filled
